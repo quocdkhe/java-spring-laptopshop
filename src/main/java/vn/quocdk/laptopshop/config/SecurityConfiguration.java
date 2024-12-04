de6bb13a1095d6c2e4e60b08a262b121cecaa0ba
quocdk <doanquoc281004@gmail.com>
2024-12-04 18:54:50 +0700

    more spring session

--- src/main/java/vn/quocdk/laptopshop/config/SecurityConfiguration.java
+++ src/main/java/vn/quocdk/laptopshop/config/SecurityConfiguration.java
@@ -5,11 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.session.security.web.authentication.SpringSessionRememberMeServices;
 
 import jakarta.servlet.DispatcherType;
 import vn.quocdk.laptopshop.service.UserService;
@@ -41,6 +43,14 @@ public class SecurityConfiguration {
     }
 
     @Bean
+    public SpringSessionRememberMeServices rememberMeServices() {
+        SpringSessionRememberMeServices rememberMeServices = new SpringSessionRememberMeServices();
+        // optionally customize
+        rememberMeServices.setAlwaysRemember(true);
+        return rememberMeServices;
+    }
+
+    @Bean
     public AuthenticationSuccessHandler customSuccessAuthHandler() {
         return new CustomSuccessHandler();
     }
@@ -59,13 +69,21 @@ public class SecurityConfiguration {
                         .requestMatchers("/admin/**").hasRole("Admin")
 
                         .anyRequest().authenticated())
-
+                .sessionManagement((sessionManagement) -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+                        .invalidSessionUrl("/logout?expired")
+                        .maximumSessions(1)
+                        .maxSessionsPreventsLogin(false))
+                .logout(logout->logout.deleteCookies("JSESSIONID").invalidateHttpSession(true))
+                
+                .rememberMe((rememberMe) -> rememberMe
+                        .rememberMeServices(rememberMeServices()))
                 .formLogin(formLogin -> formLogin
                         .loginPage("/login")
                         .failureUrl("/login?error")
                         .successHandler(customSuccessAuthHandler())
                         .permitAll())
-                        .exceptionHandling(exception -> exception.accessDeniedPage("/access-denied"));
+                .exceptionHandling(exception -> exception.accessDeniedPage("/access-denied"));
         return http.build();
     }
 
